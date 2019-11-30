4b7c7a2e4c7855cd516daec21cbc0a61d415a6b1
JackMeagher01 <56548581+JackMeagher01@users.noreply.github.com>
2019-11-30 09:38:19 +0000

    Add files via upload

---
+++ website/form.js
@@ -0,0 +1,11 @@
+function openLoginForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeSignUpForm() { 
+    window.location.replace("homepage.html");   
+}
+
+function closeLoginForm() {
+    document.getElementById("myForm").style.display = "none";
+}
